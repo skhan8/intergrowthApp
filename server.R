70dbfe5492eb740ddf93aeee072b645d2cc3bf7f
skhan8 <skhan844844@gmail.com>
2016-06-29 14:01:37 -0400

    inital commit

---
+++ server.R
@@ -0,0 +1,109 @@
+library(hbgd)
+shinyServer(function(input, output) {
+  
+  output$value <- renderPrint({input$percentile})
+  
+  
+  centileCalc<-eventReactive(input$goButton2, {
+    
+#     # if centile is Female
+# 
+# #     if((input$sexCentile)=="2"){
+# #       tempCentile<-igb_centile2hcircm(input$gagebrthCentile, p = input$percentile, sex = "Female")}
+# #     # if centile is Male
+# #     if((input$sexCentile)=="1"){
+# #       tempCentile<-igb_centile2hcircm(input$gagebrthCentile, p = input$percentile, sex = "Male")}
+# #    
+#     
+#  tempCentile<-igb_centile2hcircm(input$gagebrthCentile, p = input$percentile, sex = input$sexCentile)
+#     
+#     return(tempCentile)
+#     
+   })
+  
+  output$centileOutput<-renderText({
+    if(input$choiceCentile==1){
+      measurement<-"head circumference (cm) "
+    tempCentile<-igb_centile2hcircm(input$gagebrthCentile, p = input$percentile, sex = input$sexCentile)
+    }
+    if(input$choiceCentile==2){
+      measurement<-"length (cm)"
+      tempCentile<-igb_centile2lencm(input$gagebrthCentile, p = input$percentile, sex = input$sexCentile)
+    }
+    if(input$choiceCentile==3){
+      measurement<-"weight (kg)"
+      tempCentile<-igb_centile2wtkg(input$gagebrthCentile, p = input$percentile, sex = input$sexCentile)
+    }
+   # return(tempCentile)
+    return(c("A",input$sexCentile, "baby who is ", input$gagebrthCentile, "days old under the", paste0(input$percentile, "th Centile has a"), measurement, "of", round(tempCentile, 3)))
+   
+  })
+  
+  
+  ## Loading in the csv file
+  datasetInput <- eventReactive(input$goButton, {
+    
+    # if there is no csv file, there will be no output
+    if (is.null(input$file1))
+      return(NULL)
+    
+    fileRead <- input$file1
+    
+    inFile<-read.csv(fileRead$datapath, header=T)
+    
+    ### DATA CLEANING/VALIDATION ###
+    
+    # taking out Rows that have a gestat. birth that is outside the realm of the calculator, 
+    # or if it's not numeric and saving them for later as "badRows"
+    badRows<-inFile[(inFile$gagebrth>300) | (inFile$gagebrth<=232) | !is.numeric(inFile$gagebrth),]
+    
+    #keeping the good rows that can go ahead and be pushed through the calc. 
+    goodRows<-inFile[!((inFile$gagebrth>300) | (inFile$gagebrth<=232) | !is.numeric(inFile$gagebrth)),]
+    
+    
+    if((input$len)==TRUE){
+      goodRows$lenZScore <- igb_lencm2zscore(goodRows$gagebrth, goodRows$lencm, sex = goodRows$sex)
+      
+    }
+    
+    if((input$wt)==TRUE){
+      goodRows$wtZScore <- igb_wtkg2zscore(goodRows$gagebrth, goodRows$wtkg, sex = goodRows$sex)
+      
+    }
+    
+    if((input$headcm)==TRUE){
+      goodRows$headZScore <- igb_hcircm2zscore(goodRows$gagebrth, goodRows$hcircm, sex = goodRows$sex)
+    }
+    
+    
+    # adding back the "badRows" 
+    mydata <- merge(goodRows, badRows, by=c("subjid", "sex" ,"gagebrth", "hcircm" ,"wtkg" ,"lencm"),
+                    all=TRUE)
+    
+    return(mydata)
+    
+  })
+  
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  
+  ## this is for illustrative purposes to show what format the files should be in
+  output$table2 <- renderTable({
+    header  = c("subjid",  "sex",	"gagebrth",	"hcircm",	"wtkg",	"lencm") 
+    format  = c("Numeric","Male or Female","Gestational age at birth (days)","Head circumference (cm)",
+                "weight (kg)", "Recumbent length (cm)")
+    df = data.frame(header, format) 
+    
+  })
+  
+  ## an option to download the generated csv
+  output$downloadData <- downloadHandler(
+ 
+    filename = function() { paste0("IntergrowthFile_",Sys.Date(), '.csv') },
+    content = function(file) {
+      write.csv(datasetInput(), file)
+    }
+  )
+})
---
+++ ui.R
@@ -0,0 +1,76 @@
+library(hbgd)
+
+shinyUI(navbarPage("Intergrowth CDC", 
+                   tabPanel("Intergrowth Birth Standards (232 to 300 days)",
+                            pageWithSidebar(
+                              
+                              
+                              headerPanel(''),
+                              sidebarPanel(    
+                                h4(p(style = "color:dodgerblue","Step 1.")),
+                                "Prepare the data file like so:",hr(),
+                                
+                                tableOutput('table2'),
+                                #     img(src = 'capture.PNG', align = "center"),
+                                #     hr(),
+                                h4(p(style = "color:dodgerblue","Step 2.")),
+                                "Upload the data file (only .csv accepted)",
+                                hr(),
+                                fileInput('file1', 'Choose CSV File',
+                                          accept=c('text/csv', 
+                                                   'text/comma-separated-values,text/plain', 
+                                                   '.csv')),
+                                hr(),
+                                checkboxInput('headcm', 'Head Circumference'),
+                                checkboxInput('wt', 'Weight'),
+                                checkboxInput('len', 'Length'),
+                                actionButton("goButton", "Start Analysis!")
+                                #,
+                                # radioButtons('mm', 'Measurement:',
+                                #              c("Centile" = 1, "Z-Score" = 2))
+                                
+                              ),
+                              mainPanel(  h4(p(style = "color:dodgerblue","Output")),
+                                          hr(),
+                                          
+                                          
+                                          tableOutput('table'),
+                                          
+                                          h4(p(style = "color:dodgerblue","Download the data")),
+                                          downloadButton('downloadData', 'Download')
+                              )
+                            )),
+                   tabPanel("Centile calculator (232 to 300 days)",
+                            pageWithSidebar(
+                              headerPanel('Centile calculator'),
+                              
+                              sidebarPanel(sliderInput("percentile", label = h3("Pick a centile:"), 
+                                                       min = 0.01, 
+                                                       max = 99.99, value = 50),
+                                           h5(p(style = "color:dodgerblue","You've selected this centile:")),
+                                           verbatimTextOutput("value"),
+                                           
+                                           radioButtons("sexCentile", label = h3("Sex:"),
+                                                        choices = list("Male" = "Male", "Female" = "Female"), 
+                                                        selected = "Male"),
+                                           
+                                           
+                                           numericInput("gagebrthCentile", label = h3("Gestational birth in days (232-300)"),
+                                                        min=231, max=300, value=270)
+#                                            ,
+#                  
+#                                            actionButton("goButton2", "Start Analysis!")
+                                           
+                                           
+                              ),
+                              mainPanel(                            
+                                
+                                radioButtons("choiceCentile", label = h3("Choose a measurement:"),
+                                             choices = list("Head circ. (cm)" =  1, "Length (cm)" = 2, 
+                                                            "Weight (kg)"= 3), selected = 1),
+                                hr(),br(),br(),br(),
+                                h4(p(style = "color:dodgerblue","The result:")),
+                                          verbatimTextOutput("centileOutput"))
+                            )
+                   )
+))
